e4df804982672338ec91c788be6bf160899285c0
jgillar <jamie.gillar@gmail.com>
2019-11-12 22:49:48 -0500

    basic solution to Two Sum done

--- 1 two sum.js
+++ 1 two sum.js
@@ -4,6 +4,21 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 let twoSum = function(nums, target) {
+  let result = null;
+  nums.forEach((num1, index1) => {
+    nums.forEach((num2, index2) => {
+      if (num1 + num2 === target) {
+        result = [index1, index2];
+        return;
+      }
+    });
+  });
 
+  return result;
 };
