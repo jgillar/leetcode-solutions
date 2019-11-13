d2cd5663731a49b9493470de9e372de14b5a0f75
jgillar <jamie.gillar@gmail.com>
2019-11-12 22:58:05 -0500

    setting up for 2nd question

---
+++ 2 add two numbers.js
@@ -0,0 +1,22 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+*/
+
+class Node {
+  constructor(data, next = null) {
+    (this.data = data), (this.next = next);
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {};
