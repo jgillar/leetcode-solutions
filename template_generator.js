191d1ffc94cd2e398604ec2301cf8d9c9d064871
jgillar <jamie.gillar@gmail.com>
2020-01-24 11:57:38 -0500

    updated the template generator so the dialog box can be closed

--- template_generator.js
+++ template_generator.js
@@ -1,12 +1,14 @@
-//this is a simple script to generate a template from any leetcode exercise
-//basically it just pulls the description text and title
-var makeTemplate = () => {
-  let title = document.querySelector("[data-cy='question-title']").textContent;
-  let filename = title.toLocaleLowerCase().replace(".", "");
-  let description = document.querySelector(
-    "div[data-cy='description-content'] > div > div:nth-child(2)"
-  ).innerHTML;
-  let template = `<!DOCTYPE html>
+(function() {
+  //this is a simple script to generate a template from any leetcode exercise
+  //basically it just pulls the description text and title
+  let makeTemplate = () => {
+    let title = document.querySelector("[data-cy='question-title']")
+      .textContent;
+    let filename = title.toLocaleLowerCase().replace(".", "");
+    let description = document.querySelector(
+      "div[data-cy='description-content'] > div > div:nth-child(2)"
+    ).innerHTML;
+    let template = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -25,37 +27,41 @@ var makeTemplate = () => {
     <title>${title}</title>
   </head>
   <body>
-    <h1>${title}</h1>
-	<section>
-		${description}
-	</section>
-	<section>
-		<pre> Example goes here! </pre>
-	</section>
-	<section>
-      <p>
-        Analysis goes here!
-      </p>
-    </section>
+	<main>
+		<h1>${title}</h1>
+		<section>
+			<h2>Description</h2>
+			${description}
+		</section>
+		<section>
+			<h2>My Solution</h2>
+			<h3>Example Testcase</h3>
+			<pre> Example goes here! </pre>
+			<h3>Analysis</h3>
+			<p>
+				Analysis goes here!
+			</p>
+		</section>
+	</main>
   </body>
 </html>`;
-  let dialog = `
+    let dialog = `
 <div id="lcs-dialog">
 	  <section>
 		  <label for="lcs-markup">Template markup:</label>
 		  <textarea name="lcs-markup">${template}</textarea>
 		  <label for="lcs-filename">Filename:</label>
-		  <input type="text for="lcs-filename" value="${filename}" />
+		  <input type="text" name="lcs-filename" value="${filename}" />
 	  </section>
 	  <section>
-	  	<button onClick="close()">Close</button>
+	  	<button id="lcs-close">Close</button>
 	  </section>
 </div>`;
 
-  return dialog;
-};
+    return dialog;
+  };
 
-var styles = `
+  let styles = `
 	<style type="text/css">
 		#lcs-dialog{
 			background: #cccccc;
@@ -64,6 +70,7 @@ var styles = `
 			padding: 25px;
 			position: absolute;
 			top: 50%;
+			transition: opacity .5s;
 			transform: translate(-50%, -50%);
 			width: 400px;
 			z-index: 100;
@@ -79,6 +86,18 @@ var styles = `
 		}
 	</style>
 `;
-document
-  .getElementsByTagName("body")[0]
-  .insertAdjacentHTML("beforeend", makeTemplate() + " " + styles);
+  let dialog = document.getElementById("lcs-dialog");
+  if (dialog) {
+    dialog.style.opacity = 1;
+  } else {
+    document
+      .getElementsByTagName("body")[0]
+      .insertAdjacentHTML("beforeend", makeTemplate() + " " + styles);
+    dialog = document.getElementById("lcs-dialog");
+  }
+  document.addEventListener("click", function(e) {
+    if (e.target && e.target.id == "lcs-close") {
+      dialog.style.opacity = 0;
+    }
+  });
+})();
