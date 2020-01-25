c65480d82276866b7e76289f3ca187a346013b1a
jgillar <jamie.gillar@gmail.com>
2020-01-24 20:43:48 -0500

    updated template generator to include library file

--- template_generator.js
+++ template_generator.js
@@ -15,6 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<link rel="stylesheet" href="styles.css">
+	<script type="text/javascript" src="common.js"></script>
     <script
       type="text/javascript"
       src="${filename}.js"
